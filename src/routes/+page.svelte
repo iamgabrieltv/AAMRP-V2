1a83e5ce260674ec03b215bc774b83fa9df7fdf7
iamgabrieltv <gabrielsarcevic1@gmail.com>
2026-04-26 22:57:17 +0200

    huh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
   import { onMount, onDestroy } from "svelte";
   import { start, stop, setActivity } from "tauri-plugin-drpc";
   import { Activity, ActivityType, Assets } from "tauri-plugin-drpc/activity";
@@ -19,11 +20,26 @@
     .setDetails("Testing Tauri")
     .setState("It works!");
 
+  const json = {
+    state: "Artist Name",
+    details: "Song Name",
+    activity_type: 2,
+    assets: {
+      large_image: "dummy",
+      large_text: "Album Cover",
+      small_image: "dummy",
+      small_text: "Artist",
+    },
+  };
+
   onMount(async () => {
     await start("1423726101519274056");
-    setTimeout(async () => {
-      await setActivity(simpleActivity);
-    }, 500);
+    await invoke("plugin:drpc|set_activity", {
+      activityJson: JSON.stringify(json),
+    });
+    // setTimeout(async () => {
+    //   await setActivity(activity);
+    // }, 500);
   });
 
   onDestroy(async () => {
