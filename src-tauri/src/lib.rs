782c42122979a39c467ba86f7357e6cac0b8be41
iamgabrieltv <gabrielsarcevic1@gmail.com>
2026-05-08 13:29:25 +0200

    Update capability platforms

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -121,6 +121,7 @@ async fn apple_request(title: String, artist: String, album: String) -> Result<V
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_os::init())
